55703964e4c03a0c34b9cefe76ed5cb5490af6e2
Hillary <hkrumbho@gmail.com>
2023-03-05 16:32:14 -0800

    Adding R script that shows file.path example

---
+++ file_path_example.R
@@ -0,0 +1,3 @@
+my_path <- file.path("C:", "Users", "krumb", "OneDrive", "Desktop", "Git_that_fish")
+
+fish_species <- read.csv(file = file.path(my_path, "MCR_LTER_Fish_Species_List_20220120.csv"))
