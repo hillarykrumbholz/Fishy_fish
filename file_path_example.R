1684920cd4f0cec06ee6c76298aa62928e488b75
Hillary <hkrumbho@gmail.com>
2023-03-05 17:22:41 -0800

    adding dir.create funtion

--- file_path_example.R
+++ file_path_example.R
@@ -1,3 +1,6 @@
-my_path <- file.path("C:", "Users", "krumb", "OneDrive", "Desktop", "Git_that_fish")
+my_path <- file.path("C:", "Users", "krumb", "OneDrive", "Desktop", "Git_that_fish", "Fishy_fish")
 
 fish_species <- read.csv(file = file.path(my_path, "MCR_LTER_Fish_Species_List_20220120.csv"))
+
+
+dir.create(path = file.path(my_path, "Plots"))
